25320ab49ab673e3fee80a98e377b8a3f5ec8c10
Andrii <andkryvenko@gmail.com>
2023-12-22 11:46:41 +0200

    create countdown

---
+++ index.js
@@ -0,0 +1,34 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
+  }
+
+  timer = setInterval(() => {
+    const time = this.targetDate - Date.now();
+    this.updateClockface(time);
+  }, 1000);
+
+  updateClockface(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    this.selector.querySelector('[data-value= "days"]').textContent = days;
+    this.selector.querySelector('[data-value="hours"]').textContent = hours;
+    this.selector.querySelector('[data-value="mins"]').textContent = mins;
+    this.selector.querySelector('[data-value="secs"]').textContent = secs;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+new CountdownTimer({
+  selector: "#timer",
+  targetDate: new Date("Dec 23, 2023, 19:30:00"),
+});
